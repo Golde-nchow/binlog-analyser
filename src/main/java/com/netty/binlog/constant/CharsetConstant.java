ee2fed727d5ea109d083071d800fdcd1385f9f87
Golde-nchow <570384231@qq.com>
2021-03-21 23:57:57 +0800

    mysql 编码常量储存10进制值

--- src/main/java/com/netty/binlog/constant/CharsetConstant.java
+++ src/main/java/com/netty/binlog/constant/CharsetConstant.java
@@ -2,11 +2,11 @@ package com.netty.binlog.constant;
 
 /**
  * @author by chow
- * @Description mysql 编码16进制
+ * @Description mysql 编码（10进制）
  * @date 2021/3/18 下午11:31
  */
 public interface CharsetConstant {
 
-    int UTF8_GENERAL_CI = 21;
+    int UTF8_GENERAL_CI = 33;
 
 }
