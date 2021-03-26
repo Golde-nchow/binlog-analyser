135f974aeef814869d10e75488d6305db9b40079
Golde-nchow <570384231@qq.com>
2021-03-26 21:48:14 +0800

    添加编码枚举类

--- src/main/java/com/netty/binlog/constant/CharsetConstant.java
+++ src/main/java/com/netty/binlog/constant/CharsetConstant.java
@@ -5,8 +5,649 @@ package com.netty.binlog.constant;
  * @Description mysql 编码（10进制）
  * @date 2021/3/18 下午11:31
  */
-public interface CharsetConstant {
+public enum CharsetConstant {
 
-    int UTF8_GENERAL_CI = 33;
+    /**
+     * 1
+     */
+    BIG5_CHINESE_CI,
 
+    /**
+     * 2
+     */
+    LATIN2_CZECH_CS,
+
+    /**
+     * 3
+     */
+    DEC8_SWEDISH_CI,
+
+    /**
+     * 4
+     */
+    CP850_GENERAL_CI,
+
+    /**
+     * 5
+     */
+    LATIN1_GERMAN1_CI,
+
+    /**
+     * 6
+     */
+    HP8_ENGLISH_CI,
+
+    /**
+     * 7
+     */
+    KOI8R_GENERAL_CI,
+
+    /**
+     * 8
+     */
+    LATIN1_SWEDISH_CI,
+
+    /**
+     * 9
+     */
+    LATIN2_GENERAL_CI,
+
+    /**
+     * 10
+     */
+    SWE7_SWEDISH_CI,
+
+    /**
+     * 11
+     */
+    ASCII_GENERAL_CI,
+
+    /**
+     * 12
+     */
+    UJIS_JAPANESE_CI,
+
+    /**
+     * 13
+     */
+    SJIS_JAPANESE_CI,
+
+    /**
+     * 14
+     */
+    CP1251_BULGARIAN_CI,
+
+    /**
+     * 15
+     */
+    LATIN1_DANISH_CI,
+
+    /**
+     * 16
+     */
+    HEBREW_GENERAL_CI,
+
+    /**
+     * 17
+     */
+    TIS620_THAI_CI,
+
+    /**
+     * 18
+     */
+    EUCKR_KOREAN_CI,
+
+    /**
+     * 19
+     */
+    LATIN7_ESTONIAN_CS,
+
+    /**
+     * 20
+     */
+    LATIN2_HUNGARIAN_CI,
+
+    /**
+     * 21
+     */
+    KOI8U_GENERAL_CI,
+
+    /**
+     * 22
+     */
+    CP1251_UKRAINIAN_CI,
+
+    /**
+     * 23
+     */
+    GB2312_CHINESE_CI,
+
+    /**
+     * 24
+     */
+    GREEK_GENERAL_CI,
+
+    /**
+     * 25
+     */
+    CP1250_GENERAL_CI,
+
+    /**
+     * 26
+     */
+    LATIN2_CROATIAN_CI,
+
+    /**
+     * 27
+     */
+    GBK_CHINESE_CI,
+
+    /**
+     * 28
+     */
+    CP1257_LITHUANIAN_CI,
+
+    /**
+     * 29
+     */
+    LATIN5_TURKISH_CI,
+
+    /**
+     * 30
+     */
+    LATIN1_GERMAN2_CI,
+
+    /**
+     * 31
+     */
+    ARMSCII8_GENERAL_CI,
+
+    /**
+     * 32
+     */
+    UTF8_GENERAL_CI,
+
+    /**
+     * 33
+     */
+    CP1250_CZECH_CS,
+
+    /**
+     * 34
+     */
+    UCS2_GENERAL_CI,
+
+    /**
+     * 35
+     */
+    CP866_GENERAL_CI,
+
+    /**
+     * 36
+     */
+    KEYBCS2_GENERAL_CI,
+
+    /**
+     * 37
+     */
+    MACCE_GENERAL_CI,
+
+    /**
+     * 38
+     */
+    MACROMAN_GENERAL_CI,
+
+    /**
+     * 39
+     */
+    CP852_GENERAL_CI,
+
+    /**
+     * 40
+     */
+    LATIN7_GENERAL_CI,
+
+    /**
+     * 41
+     */
+    LATIN7_GENERAL_CS,
+
+    /**
+     * 42
+     */
+    MACCE_BIN,
+
+    /**
+     * 43
+     */
+    CP1250_CROATIAN_CI,
+
+    /**
+     * 44
+     */
+    LATIN1_BIN,
+
+    /**
+     * 45
+     */
+    LATIN1_GENERAL_CI,
+
+    /**
+     * 46
+     */
+    LATIN1_GENERAL_CS,
+
+    /**
+     * 47
+     */
+    CP1251_BIN,
+
+    /**
+     * 48
+     */
+    CP1251_GENERAL_CI,
+
+    /**
+     * 49
+     */
+    CP1251_GENERAL_CS,
+
+    /**
+     * 50
+     */
+    MACROMAN_BIN,
+
+    /**
+     * 51
+     */
+    CP1256_GENERAL_CI,
+
+    /**
+     * 52
+     */
+    CP1257_BIN,
+
+    /**
+     * 53
+     */
+    CP1257_GENERAL_CI,
+
+    /**
+     * 54
+     */
+    BINARY,
+
+    /**
+     * 55
+     */
+    ARMSCII8_BIN,
+
+    /**
+     * 56
+     */
+    ASCII_BIN,
+
+    /**
+     * 57
+     */
+    CP1250_BIN,
+
+    /**
+     * 58
+     */
+    CP1256_BIN,
+
+    /**
+     * 59
+     */
+    CP866_BIN,
+
+    /**
+     * 60
+     */
+    DEC8_BIN,
+
+    /**
+     * 61
+     */
+    GREEK_BIN,
+
+    /**
+     * 62
+     */
+    HEBREW_BIN,
+
+    /**
+     * 63
+     */
+    HP8_BIN,
+
+    /**
+     * 64
+     */
+    KEYBCS2_BIN,
+
+    /**
+     * 65
+     */
+    KOI8R_BIN,
+
+    /**
+     * 66
+     */
+    KOI8U_BIN,
+
+    /**
+     * 67
+     */
+    LATIN2_BIN,
+
+    /**
+     * 68
+     */
+    LATIN5_BIN,
+
+    /**
+     * 69
+     */
+    LATIN7_BIN,
+
+    /**
+     * 70
+     */
+    CP850_BIN,
+
+    /**
+     * 71
+     */
+    CP852_BIN,
+
+    /**
+     * 72
+     */
+    SWE7_BIN,
+
+    /**
+     * 73
+     */
+    UTF8_BIN,
+
+    /**
+     * 74
+     */
+    BIG5_BIN,
+
+    /**
+     * 75
+     */
+    EUCKR_BIN,
+
+    /**
+     * 76
+     */
+    GB2312_BIN,
+
+    /**
+     * 77
+     */
+    GBK_BIN,
+
+    /**
+     * 78
+     */
+    SJIS_BIN,
+
+    /**
+     * 79
+     */
+    TIS620_BIN,
+
+    /**
+     * 80
+     */
+    UCS2_BIN,
+
+    /**
+     * 81
+     */
+    UJIS_BIN,
+
+    /**
+     * 82
+     */
+    GEOSTD8_GENERAL_CI,
+
+    /**
+     * 83
+     */
+    GEOSTD8_BIN,
+
+    /**
+     * 84
+     */
+    LATIN1_SPANISH_CI,
+
+    /**
+     * 85
+     */
+    CP932_JAPANESE_CI,
+
+    /**
+     * 86
+     */
+    CP932_BIN,
+
+    /**
+     * 87
+     */
+    EUCJPMS_JAPANESE_CI,
+
+    /**
+     * 88
+     */
+    EUCJPMS_BIN,
+
+    /**
+     * 89
+     */
+    UCS2_UNICODE_CI,
+
+    /**
+     * 90
+     */
+    UCS2_ICELANDIC_CI,
+
+    /**
+     * 91
+     */
+    UCS2_LATVIAN_CI,
+
+    /**
+     * 92
+     */
+    UCS2_ROMANIAN_CI,
+
+    /**
+     * 93
+     */
+    UCS2_SLOVENIAN_CI,
+
+    /**
+     * 94
+     */
+    UCS2_POLISH_CI,
+
+    /**
+     * 95
+     */
+    UCS2_ESTONIAN_CI,
+
+    /**
+     * 96
+     */
+    UCS2_SPANISH_CI,
+
+    /**
+     * 97
+     */
+    UCS2_SWEDISH_CI,
+
+    /**
+     * 98
+     */
+    UCS2_TURKISH_CI,
+
+    /**
+     * 99
+     */
+    UCS2_CZECH_CI,
+
+    /**
+     * 100
+     */
+    UCS2_DANISH_CI,
+
+    /**
+     * 101
+     */
+    UCS2_LITHUANIAN_CI,
+
+    /**
+     * 102
+     */
+    UCS2_SLOVAK_CI,
+
+    /**
+     * 103
+     */
+    UCS2_SPANISH2_CI,
+
+    /**
+     * 104
+     */
+    UCS2_ROMAN_CI,
+
+    /**
+     * 105
+     */
+    UCS2_PERSIAN_CI,
+
+    /**
+     * 106
+     */
+    UCS2_ESPERANTO_CI,
+
+    /**
+     * 107
+     */
+    UCS2_HUNGARIAN_CI,
+
+    /**
+     * 108
+     */
+    UTF8_UNICODE_CI,
+
+    /**
+     * 109
+     */
+    UTF8_ICELANDIC_CI,
+
+    /**
+     * 110
+     */
+    UTF8_LATVIAN_CI,
+
+    /**
+     * 111
+     */
+    UTF8_ROMANIAN_CI,
+
+    /**
+     * 112
+     */
+    UTF8_SLOVENIAN_CI,
+
+    /**
+     * 113
+     */
+    UTF8_POLISH_CI,
+
+    /**
+     * 114
+     */
+    UTF8_ESTONIAN_CI,
+
+    /**
+     * 115
+     */
+    UTFZ8_SPANISH_CI,
+
+    /**
+     * 116
+     */
+    UTF8_SWEDISH_CI,
+
+    /**
+     * 117
+     */
+    UTF8_TURKISH_CI,
+
+    /**
+     * 118
+     */
+    UTF8_CZECH_CI,
+
+    /**
+     * 119
+     */
+    UTF8_DANISH_CI,
+
+    /**
+     * 120
+     */
+    UTF8_LITHUANIAN_CI,
+
+    /**
+     * 121
+     */
+    UTF8_SLOVAK_CI,
+
+    /**
+     * 122
+     */
+    UTF8_SPANISH2_CI,
+
+    /**
+     * 123
+     */
+    UTF8_ROMAN_CI,
+
+    /**
+     * 124
+     */
+    UTF8_PERSIAN_CI,
+
+    /**
+     * 125
+     */
+    UTF8_ESPERANTO_CI,
+
+    /**
+     * 126
+     */
+    UTF8_HUNGARIAN_CI,
+    ;
+
+    CharsetConstant() {
+    }
+
+    /**
+     * 通过索引，获取对应的值
+     * @param index 索引
+     * @return 对应的值
+     */
+    public static String get(int index) {
+        CharsetConstant[] values = values();
+        return values[index].name();
+    }
 }
